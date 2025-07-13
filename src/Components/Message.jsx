df5b6aceddc841acd405a24b47bd08b71acc77b7
hibaanadani <hibaanadani1@gmail.com>
2025-07-13 22:12:35 +0300

    implemented props in messages related parts

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,10 +1,22 @@
+import PropTypes from 'prop-types'
+
 function Message(props){
     return(
-        <div>
+        <div className="message">
             <p className="renderedMessage">{props.message}</p> 
             <h3 className="senderName"> {props.name}</h3>
             <p className="opened">{props.isOpened ? "Yes" : "No"}</p>
         </div>
     );
+}
+Message.protoTypes ={
+    message: PropTypes.string,
+    name: PropTypes.string,
+    isOpened: PropTypes.bool,
+}
+Message.defaultProps ={
+    message: "Nothing to read",
+    name: "Username",
+    isOpened: true,
 }
 export default Message
