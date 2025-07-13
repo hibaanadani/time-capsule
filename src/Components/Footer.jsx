878e0efa7231e99b11dc7a8504c4b39625f87398
hibaanadani <hibaanadani1@gmail.com>
2025-07-13 15:09:24 +0300

    created the footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return(
+        <footer>
+           <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <p>&copy;{new Date().getFullYear()} MessageToSelf</p>
+                    </ul>
+        </footer>
+    );
+}
+export default Footer
