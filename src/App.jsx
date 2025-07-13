c75fcb6e2a4bbf28737403abb5c910edbdb2f337
hibaanadani <hibaanadani1@gmail.com>
2025-07-13 14:25:44 +0300

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import Header from './Components/Header';
+
+
+function App() {
+  return <Header></Header>
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <header>
+            <img src="" alt="" />
+            <h1>MessageToSelf</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <button>Log In</button>
+                    <button>Create Your Time Capsule</button>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
