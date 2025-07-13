9067031cae309f746483b1b6c344af60a52904a9
hibaanadani <hibaanadani1@gmail.com>
2025-07-13 15:24:37 +0300

    Integrated footer component and included root project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
-import Header from './Components/Header';
+import Header from './Components/Header.jsx';
+import Footer from './Components/Footer.jsx';
 
 
 function App() {
-  return <Header></Header>
+  return( 
+    <>
+  <Header/>
+  <Footer/>
+  </>
+)
 }
 
 export default App;
