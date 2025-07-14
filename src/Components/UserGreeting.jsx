94c327658083ab5134b1d974064123cca3f437f6
hibaanadani <hibaanadani1@gmail.com>
2025-07-14 10:29:27 +0300

    fixed propTypes in UserFreetings

--- src/Components/UserGreeting.jsx
+++ src/Components/UserGreeting.jsx
@@ -4,17 +4,16 @@ function UserGreeting(props){
     const welcomeMessage = <h2 className="welcome-message">Hello {props.username}!</h2>
     const loginPrompt =  <h2 className="login-prompt"> Please Log in to continue!</h2>
     
-    return(props.isLoggedIn ?  welcomeMessage : loginPrompt
-   );
+    return(props.isLoggedIn ?  welcomeMessage : loginPrompt);
 
 } 
-UserGreeting.PropTypes ={
+UserGreeting.propTypes = {
     isLoggedIn: PropTypes.bool,
     username: PropTypes.string,
 }
 
-UserGreeting.defaultProps= {
+UserGreeting.defaultProps = {
     isLoggedIn: false,
-    username: "Guest"
+    username: "Guest",
 }
 export default UserGreeting
