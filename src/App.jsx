6d655100b40010dd8d244e0fb56bf1776ebc52f9
hibaanadani <hibaanadani1@gmail.com>
2025-07-15 16:04:45 +0300

    adjusted and added some more components

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import "./Styles/utilities.css";
 import "./Styles/colors.css";
 import "./Styles/index.css";
 import "./Styles/App.css";
+import Auth from "./Pages/Authentication"
 
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
 
 import logo from './logo.svg';
-import Header from './Components/Header.jsx';
-import Footer from './Components/Footer.jsx';
+import Header from './Components/Shared/Header/index.jsx';
+import Footer from './Components/Shared/Footer/index.jsx';
 import MessageCard from './Components/MessageCard.jsx';
 import ImageCard from './Components/ImageCard.jsx';
 import HeroSection from './Components/HeroSection.jsx';
 import UserGreeting from './Components/UserGreeting.jsx';
 
 const App = () =>{
-return <div className="App"></div>
+return <div className="App">
+  <BrowserRouter>
+  <Routes>
+    <Route path="/authentication" element={<Auth/>}/>
+    <Route path="/home" element={<Home/>}>
+    </Route>
+  </Routes>
+  </BrowserRouter>
+</div>
 };
 
 function App() {
