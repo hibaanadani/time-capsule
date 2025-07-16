0063ef2293cc53bbe299bb20ab6cf38faa38865c
hibaanadani <hibaanadani1@gmail.com>
2025-07-16 09:38:24 +0300

    fixed some errors in the signup component, and added handle login for axios fething

--- src/Components/Authntication/SignUpForm/index.jsx
+++ src/Components/Authntication/SignUpForm/index.jsx
@@ -1,14 +1,51 @@
 import Button from "../../Shared/Button";
 import Input from "../../Shared/Input";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = ({ toggle }) =>{
+    const [firstName, setFirstname] = useState();
+    const [lastname, setLastname] =useState();
+    const [username, setUsername] =useState();
+    const [email, setEmail] =useState();
+    const [password, setPassword] =useState();
+
+    const navigate = useNavigate();
+
+    const handleLogin = async() => {
+        try{
+            async () =>{
+                const res= await axios.post("loginurl", {
+                firstName:firstName,
+                lastname: lastname,
+                username: username,
+                email: email,
+                password:password,
+            });
+
+            if(true){
+                navigate("/dashboard")
+            }else{
+                alert("Signup failed, please check inputs");
+            }
+        }
+    }
+        catch (e){  
+            console.error("Login error:", e);
+            alert("an error occurred! Please try again.")
+        }
+    };
+
+
     return(
     <>
     <h1 className="auth-title">Sign Up</h1>
 
     <Input name={"firstName"} hint={"John"}/>
     <Input name={"lastName"} hint={"Doe"}/>
-    <Input name={"email"} hint={"johnDoe@mail.com"}/>
+    <Input name={"username"} hint={"johnDoe"}/>
+    <Input name={"email"} hint={"johnDoe@email.com"}/>
     <Input name={"password"} hint={"johnP@ssw0rd"}/>
     
     <p className="auth-switch">
@@ -18,7 +55,7 @@ const SignUpForm = ({ toggle }) =>{
         </span>
     </p>
 
-    <Button text={"Sign Up"}/>
+    <Button text={"Sign Up"} onClickListener={handleLogin}/>
     </>
     );
 };
