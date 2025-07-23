0f5a6dffe24cbbb87ceffa610a05e456a79868b2
hibaanadani <hibaanadani1@gmail.com>
2025-07-24 01:03:15 +0300

    auth on master

--- src/Components/Authntication/LoginForm/index.jsx
+++ src/Components/Authntication/LoginForm/index.jsx
@@ -1,34 +1,67 @@
-import { use, useEffect, useState } from "react";
+import React from "react";
+import { useState } from "react";
 import Button from "../../Shared/Button";
-import Input from "../../Shared/Input";
+import Input from "../../Shared/Input/Index.jsx";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 
 const LoginForm = ({ toggle}) =>{
-    const [username, setUsername] = useState();
-    const [password, setPassword] =useState();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
 
-    useEffect(() =>{
-       console.log("assign username") ,[username]});
+    const handleLogin = async() => {
+        if (!username || !password) {
+            toast.warn("Please enter both username and password.");
+            return; 
+        }
+        try{
+            const res= await axios.post("http://localhost:8000/api/login", {
+                username: username,
+                password:password,
+            });
 
-       return(
+            if (res.status === 200) {
+                localStorage.setItem('token', res.data.payload.token);
+                localStorage.setItem('user_id', res.data.payload.id);
+                toast.success("Login successful!");
+                setTimeout(() => {
+                    navigate("/dashboard");
+                }, 1000); 
+            }else{
+                toast.error("Login failed, please check credentials");
+            }
+        }
+        catch (e){  
+            const errorMessage=  e.response?.data?.message
+            console.error("Login error:",errorMessage,e);
+            toast.error("an error occurred! Please try again.")
+        }
+    };
+
+    return(
         <>
-        <h1 className="auth-title"> Log In</h1>
+        <h2 className="auth-title"> Log In</h2>
         <Input
-        username={"username"}
-        hint={"johnDoe"}
+        name={"username"}
+        hint={"Username: johnDoe"}
+        value={username}
         onChangeListener={(e) =>{
             setUsername(e.target.value);
         }}
+        required={true}
         />
         <Input
         name={"password"}
-        hint={"johnP@ssw0rd"}
+        hint={"Password: johnP@ssw0rd"}
+        type={"password"}
+        value={password}
         onChangeListener={(e) =>{
-            setUsername(e.target.value);
+            setPassword(e.target.value);
         }}
+        required={true}
         />
         <p className="auth-switch">
             Don't have an account? 
@@ -38,18 +71,8 @@ const LoginForm = ({ toggle}) =>{
         </p>
         <Button
             text={"Log In"}
-            onClickListener={async () =>{
-                const res= await axios.post("loginurl", {
-                username: username,
-                pass:password,
-            });
-
-            if(true){
-                navigate("/dashboard")
-            }else{
-                alert("Login failed, please check credentials");
-            }
-        }}
+            onClickListener={handleLogin}
+            buttonType={"authB"}
         />
         </>
        );
